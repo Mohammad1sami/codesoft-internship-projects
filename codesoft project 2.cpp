7371bf4c4b8a14879b8cb17feea92f5eaf28f649
Mohammad Sami Shaikh <116106879+Mohammad1sami@users.noreply.github.com>
2024-02-02 11:11:02 +0530

    codesoft project 2.cpp

--- codesoft project 2.cpp
+++ codesoft project 2.cpp
@@ -1,3 +1,6 @@
+//welcome to the simple calculator program,
+//This program will take two numbers as operand and a operator from user
+// And perform operations(+,-,*,/) on the mentioned operands.
 #include <iostream>
 using namespace std;
 int main() {
@@ -12,17 +15,21 @@ int main() {
         cin >> operation;
         switch (operation) {
             case '+':
-                cout << num1 << " + " << num2 << " = " << num1 + num2 << endl; //addition
+            result=num1+num2;
+                cout <<"addition of "<<num1<<" and "<<num2<<" is "<<result<< endl; //addition
                 break;
             case '-':
-                cout << num1 << " - " << num2 << " = " << num1 - num2 << endl; //subtraction
+                result=num1-num2;
+                cout <<"subtraction of "<<num1<<" and "<<num2<<" is "<<result<< endl;//subtraction
                 break;
             case '*':
-                cout << num1 << " * " << num2 << " = " << num1 * num2 << endl; //multiplication
+              result=num1*num2;
+                cout <<"multiplication of "<<num1<<" and "<<num2<<" is "<<result<< endl; //multiplication
                 break;
             case '/':
                 if (num2 != 0)
-                    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl; //division
+                    {result=num1/num2;
+                cout <<"division of "<<num1<<" and "<<num2<<" is "<<result<< endl;} 
                 else
                     cout << "Error! Cannot divide by zero." << endl;
                 break;
